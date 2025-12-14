d558a852e874936a3e700c2f66c0071c32274923
ideefiix <flindu@gmail.com>
2025-12-14 18:42:35 +0100

    day4

---
+++ solution/Day4/Day4Solution.cs
@@ -0,0 +1,74 @@
+﻿using System.Diagnostics;
+
+namespace solution.Day4;
+
+public class Day4Solution : ISolution
+{
+    public void SolvePart1()
+    {
+        Stopwatch time = new Stopwatch();
+        time.Start();
+        var rows =
+            File.ReadAllLines(@"C:\Users\flind\Desktop\Github repos\AdventOfCode2025\solution\Day4\part1&2Input");
+        List<(int x, int y)> toiletPapperCoordinates = new List<(int x, int y)>();
+
+        for (int y = 0; y < rows.Length; y++)
+        {
+            for (int x = 0; x < rows[y].Length; x++)
+            {
+                if (rows[y][x] == '@')
+                {
+                    toiletPapperCoordinates.Add(new ValueTuple<int, int>(x, y));
+                }
+            }
+        }
+
+        var answer = toiletPapperCoordinates.Count(coord => 
+            toiletPapperCoordinates.Count(otherCoord => Math.Abs(coord.x - otherCoord.x) < 2 &&
+            Math.Abs(coord.y - otherCoord.y) < 2) < 5);
+        time.Stop();
+        Console.WriteLine("Answer: Number of pickable toiletrolls are: " + answer);
+        Console.WriteLine("Execution time was " + time.Elapsed);
+    }
+
+    public void SolvePart2()
+    {
+        Stopwatch time = new Stopwatch();
+        time.Start();
+        var rows =
+            File.ReadAllLines(@"C:\Users\flind\Desktop\Github repos\AdventOfCode2025\solution\Day4\part1&2Input");
+        List<(int x, int y)> toiletPapperCoordinates = new List<(int x, int y)>();
+
+        for (int y = 0; y < rows.Length; y++)
+        {
+            for (int x = 0; x < rows[y].Length; x++)
+            {
+                if (rows[y][x] == '@')
+                {
+                    toiletPapperCoordinates.Add(new ValueTuple<int, int>(x, y));
+                }
+            }
+        }
+
+        var deletedToiletRolls = 0;
+        
+        while (true)
+        {
+            var coordsToRemove = toiletPapperCoordinates.Where(coord =>
+                toiletPapperCoordinates.Count(otherCoord => Math.Abs(coord.x - otherCoord.x) < 2 &&
+                                                            Math.Abs(coord.y - otherCoord.y) < 2) < 5).ToList();
+            if (!coordsToRemove.Any())
+            {
+                break;
+            }
+            
+            deletedToiletRolls += coordsToRemove.Count;
+            //Console.WriteLine("Debug: Removed " + coordsToRemove.Count + " number of paperolls!");
+            toiletPapperCoordinates.RemoveAll(coord => coordsToRemove.Contains(coord));
+        }
+        
+        time.Stop();
+        Console.WriteLine("Answer: Number of removable toiletrolls are: " + deletedToiletRolls);
+        Console.WriteLine("Execution time was " + time.Elapsed);
+    }
+}
--- solution/Program.cs
+++ solution/Program.cs
@@ -1,10 +1,12 @@
 ﻿using solution.Day1;
 using solution.Day2;
 using solution.Day3;
+using solution.Day4;
 
 Day1Solution day1Solution = new Day1Solution();
 Day2Solution day2Solution = new Day2Solution();
 Day3Solution day3Solution = new Day3Solution();
+Day4Solution day4Solution = new Day4Solution();
 
 //day1Solution.SolvePart1();
 //day1Solution.SolvePart2();
@@ -13,4 +15,7 @@ Day3Solution day3Solution = new Day3Solution();
 //day2Solution.SolvePart2();
 
 //day3Solution.SolvePart1();
-day3Solution.SolvePart2();
+//day3Solution.SolvePart2();
+
+//day4Solution.SolvePart1();
+day4Solution.SolvePart2();
