132038227f07283e4e428fcd05afa9dc526b1dc5
ideefiix <flindu@gmail.com>
2025-12-14 14:18:44 +0100

    day3

---
+++ solution/Day3/Day3Solution.cs
@@ -0,0 +1,73 @@
+﻿using System.Diagnostics;
+
+namespace solution.Day3;
+
+public class Day3Solution : ISolution
+{
+    public void SolvePart1()
+    {
+        Stopwatch time = new Stopwatch();
+        time.Start();
+        var banks =
+            File.ReadAllLines(@"C:\Users\flind\Desktop\Github repos\AdventOfCode2025\solution\Day3\part1&2Input")
+                .Select(line => line.Chunk(1))
+                .Select(line => line.Select(c => int.Parse(c)).ToArray());
+        
+        Int64 totalJoltage = 0;
+        
+        foreach (var bank in banks)
+        {
+            totalJoltage += FindMaxJoltageInBank(bank, 2);
+        }
+        
+        time.Stop();
+        Console.WriteLine("Answer: The totaljoltage in the banks are " + totalJoltage);
+        Console.WriteLine("ExecutionTime: " + time.Elapsed);
+    }
+    
+    public void SolvePart2()
+    {
+        Stopwatch time = new Stopwatch();
+        time.Start();
+        var banks =
+            File.ReadAllLines(@"C:\Users\flind\Desktop\Github repos\AdventOfCode2025\solution\Day3\part1&2Input")
+                .Select(line => line.Chunk(1))
+                .Select(line => line.Select(c => int.Parse(c)).ToArray());
+        Int64 totalJoltage = 0;
+        
+        foreach (var bank in banks)
+        {
+            totalJoltage += FindMaxJoltageInBank(bank, 12);
+        }
+        time.Stop();
+        Console.WriteLine("Answer: The totaljoltage in the banks are " + totalJoltage);
+        Console.WriteLine("ExecutionTime: " + time.Elapsed);
+    }
+    
+    private Int64 FindMaxJoltageInBank(int[] bank, int numberOfBatteriesToInclude)
+    {
+        string maxJoltageInBank = "";
+        var previousHighestJoltIndex = 0;
+
+        for (int i = numberOfBatteriesToInclude; i > 0; i--)
+        {
+            var biggestJoltage = 0;
+            var nextBatteryStartPosition = 0;
+
+            for (int j = previousHighestJoltIndex; j < bank.Length -(i-1); j++)
+            {
+                if (bank[j] > biggestJoltage)
+                {
+                    biggestJoltage = bank[j];
+                    nextBatteryStartPosition = j+1;
+                }
+            }
+
+            maxJoltageInBank += biggestJoltage;
+            previousHighestJoltIndex = nextBatteryStartPosition;
+        }
+
+        //Console.WriteLine("Debug: Adding joltage " + maxJoltageInBank);
+        return long.Parse(maxJoltageInBank);
+    }
+}
--- solution/Program.cs
+++ solution/Program.cs
@@ -1,11 +1,16 @@
 ﻿using solution.Day1;
 using solution.Day2;
+using solution.Day3;
 
 Day1Solution day1Solution = new Day1Solution();
 Day2Solution day2Solution = new Day2Solution();
+Day3Solution day3Solution = new Day3Solution();
 
 //day1Solution.SolvePart1();
 //day1Solution.SolvePart2();
 
 //day2Solution.SolvePart1();
-day2Solution.SolvePart2();
+//day2Solution.SolvePart2();
+
+//day3Solution.SolvePart1();
+day3Solution.SolvePart2();
